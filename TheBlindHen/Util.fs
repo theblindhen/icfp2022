c09454d8a6dde7fa49d4088f406af8644ea2a797
Johan Rosenkilde <johanrosenkilde@github.com>
2022-09-02 20:54:51 +0200

    Score an image diff

--- TheBlindHen/Util.fs
+++ TheBlindHen/Util.fs
@@ -18,3 +18,21 @@ let median_color (img: ImageSlice) : Color =
             sum_a <- sum_a + int c.a
     let area = area img.size
     {r=byte(sum_r/area); g=byte (sum_g/area); b=byte (sum_b/area); a=byte(sum_a/area)}
+
+// Treat c1 and c2 as 4-dimensional vectors and compute the Euclidean distance
+let color_distance (c1: Color) (c2: Color) : float =
+    let r = int c1.r - int c2.r
+    let g = int c1.g - int c2.g
+    let b = int c1.b - int c2.b
+    let a = int c1.a - int c2.a
+    sqrt(float (r*r + g*g + b*b + a*a))
+
+let image_distance (proposal: ImageSlice) (target: ImageSlice) : int =
+    assert (proposal.size = target.size)
+    let mutable score = 0.0
+    for x in 0 .. proposal.size.width-1 do
+        for y in 0 .. proposal.size.height-1 do
+            let c1 = color_at_pos proposal {x=x; y=y}
+            let c2 = color_at_pos target {x=x; y=y}
+            score <- score + color_distance c1 c2
+    int (score * 0.005) // TODO: Should probably be round?
