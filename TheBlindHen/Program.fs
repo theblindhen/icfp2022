f741fd1d09eb9cda6670f2072cf8cdcd2aba56d5
Kasper Svendsen <kaspersv@gmail.com>
2022-09-03 10:04:10 +0200

    use command-line parser lib

--- TheBlindHen/Program.fs
+++ TheBlindHen/Program.fs
@@ -1,40 +1,56 @@
-﻿open Model
+﻿open Argu
+open Model
 open Loader
 open GUI
 
+type Arguments =
+    | GUI
+    | AI of AISelector option
+    | SplitPoint of SplitPointSelector option
+    | [<MainCommand; ExactlyOnce; Last>] Target of target:string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | GUI -> "Show the GUI"
+            | AI _ -> "The AI to use"
+            | SplitPoint _ -> "The split point to use"
+            | Target _ -> "The target image to use"
+
+and AISelector = OneLiner | QuadTree
+and SplitPointSelector = Midpoint | HighestDistance
+
 [<EntryPoint>]
 let main args =
-    if args.Length < 1 then
-        printfn "Usage TheBlindHen.exe [--oneline|--quadtree] <target image>"
-        1
-    else
+    let parser = ArgumentParser.Create<Arguments>(programName = "TheBlindHen.exe")
+    let results = parser.Parse args
+    let taskPath = results.GetResult (Target)
+    let task = loadPNG taskPath
     let canvas = blankCanvas {width = 400; height = 400}
-    if args[0] = "--oneline" then
+    match results.GetResult (AI) with
+    | None -> ()
+    | Some (OneLiner) ->
         /// AI dump to console
-        let taskPath = args[1]
         printfn "One-line solver on %s" taskPath
-        let task = loadPNG taskPath
         let initBlock = canvas.topBlocks |> Map.find "0"
         let solution = [ AI.colorBlockMedian (sliceWholeImage task) initBlock ]
         let solution_canvas = Instructions.simulate canvas solution
         let solution_image = renderCanvas solution_canvas
         let image_distance = Util.imageDistance (sliceWholeImage task) (sliceWholeImage solution_image)
         printfn "Score %d (TODO: Only includes image distance)\nInstructions:\n%s" (image_distance) (Instructions.deparse solution)
-        0
-    else if args[0] = "--quadtree" then
-        let taskPath = args[1]
+    | Some (QuadTree) ->
+        let splitpointSelector =
+            match results.GetResult (SplitPoint) with
+            | None -> AI.midpointCut
+            | Some (Midpoint) -> AI.midpointCut
+            | Some (HighestDistance) -> AI.highestDistanceCut
         printfn "Quadtree solver on %s" taskPath
-        let task = loadPNG taskPath
         let solution =
-            AI.quadtreeSolver
-                //AI.midpointCut
-                AI.highestDistanceCut
-                (sliceWholeImage task) canvas
+            AI.quadtreeSolver splitpointSelector (sliceWholeImage task) canvas
         printfn "Instructions:\n%s" (Instructions.deparse solution)
-        0
-    else
-        /// GUI
+    if results.Contains GUI then
+        // GUI
         let imgPath = args[0]
         let target = loadPNG imgPath
         showGui target canvas |> ignore
-        0
+    0
